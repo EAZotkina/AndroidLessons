3c09b4342d3105879708624067f898980d17c492
“[firstname <zotkinae84@gmail.com>
2021-05-31 23:57:51 +0300

    внесены правки после проверки

--- app/src/main/java/com/eazot/applicationazotlesson1/MainActivity.java
+++ app/src/main/java/com/eazot/applicationazotlesson1/MainActivity.java
@@ -3,12 +3,16 @@ package com.eazot.applicationazotlesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_texts);
+    }
+    public void switchLayout(View view) {
         setContentView(R.layout.activity_edit_texts);
     }
 }
